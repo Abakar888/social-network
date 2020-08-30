aacc9a56ca373164312155cca554a4a6dcc2adfa
Абакар <akap302@gmail.com>
2020-08-30 20:01:04 +0300

    splite css module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRzLHHnq9RV5Jl0g3xhm8rXpWCZy0qJ5KENig&usqp=CAU' />
         </header>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import classes from './Navbar.module.css';
+console.log(classes)
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div>
-                <a href='#'>Profile</a>
+        <nav className={classes.nav}>
+            <div className={classes.item}>
+                <a className={`${classes.nav__link} ${classes.active}`} href='#'>Profile</a>
             </div>
-            <div>
-                <a href='#'>Messages</a>
+            <div className={classes.item}>
+                <a className={classes.nav__link} href='#'>Messages</a>
             </div>
-            <div>
-                <a href='#'>News</a>
+            <div className={classes.item}>
+                <a className={classes.nav__link} href='#'>News</a>
             </div>
-            <div>
-                <a href='#'>Music</a>
+            <div className={classes.item}>
+                <a className={classes.nav__link} href='#'>Music</a>
             </div>
-            <div>
-                <a href='#'>Settings</a>
+            <div className={classes.item}>
+                <a className={classes.nav__link} href='#'>Settings</a>
             </div>
         </nav>
     )
