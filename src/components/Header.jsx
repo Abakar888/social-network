18fef326d8b8d8284dff65a9174d304d774913ec
Абакар <akap302@gmail.com>
2020-08-30 14:52:20 +0300

    create social-network

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRzLHHnq9RV5Jl0g3xhm8rXpWCZy0qJ5KENig&usqp=CAU' />
+        </header>
+    )
+}
+export default Header;
